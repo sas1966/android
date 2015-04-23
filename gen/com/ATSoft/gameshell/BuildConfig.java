5ce79b1bce7e6cd200d212ea0fcfb3fd2a83e5d3
sas1966 <Sasa@Aleksandars-iMac.local>
2015-04-23 16:46:48 +0200

    Version 1 of project
    Didn’t started yet :)

---
+++ gen/com/ATSoft/gameshell/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ATSoft.gameshell;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ obj/local/armeabi/objs/GameShell/GameShell.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi/objs/GameShell/GameShell.o: jni/GameShell.cpp \
+ /Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/jni.h \
+ /Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+ /Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/android/api-level.h
+
+/Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/jni.h:
+
+/Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+/Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Developer/android-ndk-r10d/platforms/android-21/arch-arm/usr/include/android/api-level.h:
